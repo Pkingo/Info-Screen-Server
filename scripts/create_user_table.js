cbcff6770f373f7527e6a94be80784deeef84ac6
Kingo <pkingo14@student.aau.dk>
2018-06-23 16:22:27 +0200

    Now uses es2015

--- scripts/create_user_table.js
+++ scripts/create_user_table.js
@@ -1,6 +1,6 @@
-import mysql from 'mysql'
-import Config from '../config';
-import Tables from '../types/table';
+var mysql = require('mysql');
+var Config = require('../config');
+var Tables = require('../types/table');
 
 var connection = mysql.createConnection({
     host        : Config.hostname,
@@ -10,10 +10,10 @@ var connection = mysql.createConnection({
     database    : Config.database,
 });
 
-connection.query('CREATE DATABASE ' + dbconfig.database);
+connection.query('CREATE DATABASE ' + Config.database);
 
 connection.query('\
-CREATE TABLE `' + dbconfig.database + '`.`' + Tables.Users + '` ( \
+CREATE TABLE `' + Config.database + '`.`' + Tables.Users + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `username` VARCHAR(20) NOT NULL, \
     `password` CHAR(60) NOT NULL, \
